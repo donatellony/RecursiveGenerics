0e46b4e975be57086f0d9cf3e13d358f68c81467
donatellony <donatellony4736@gmail.com>
2023-12-10 21:13:31 +0100

    Made the list output a little bit prettier

--- RecursiveGenericsExample/Shared/ExtensionMethods.cs
+++ RecursiveGenericsExample/Shared/ExtensionMethods.cs
@@ -6,8 +6,15 @@ public static class ExtensionMethods
 {
     internal static void AppendPointedList<T>(this StringBuilder sb, IEnumerable<T> values, string listName)
     {
-        sb.AppendLine(listName);
-        foreach (var value in values)
+        sb.AppendLine($"{listName}:");
+        var list = values.ToList();
+        if (!list.Any())
+        {
+            sb.AppendLine("* Empty! *");
+            return;
+        }
+        
+        foreach (var value in list)
         {
             sb.AppendLine($"* {value}");
         }

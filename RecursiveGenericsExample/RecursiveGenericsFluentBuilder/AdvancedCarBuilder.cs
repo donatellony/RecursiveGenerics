8e936c5888d951cbcc8ef6dd1607e9d13ae4716a
donatellony <donatellony4736@gmail.com>
2023-12-16 19:51:49 +0100

    Added the explanational comments

--- RecursiveGenericsExample/RecursiveGenericsFluentBuilder/AdvancedCarBuilder.cs
+++ RecursiveGenericsExample/RecursiveGenericsFluentBuilder/AdvancedCarBuilder.cs
@@ -2,16 +2,34 @@
 
 namespace RecursiveGenericsFluentBuilder;
 
-public class AdvancedCarBuilder : AdvancedCarBuilder<AdvancedCarBuilder>
-{
-}
-
 public class AdvancedCarBuilder<TSelf> : CarBuilder<AdvancedCarBuilder<TSelf>>
 where TSelf : AdvancedCarBuilder<TSelf>
 {
+    // Bonus example with another level of inheritance ;)
+    // Take a look at the "VeryAdvancedCarBuilder" class as well!
     public TSelf AddDoor(DoorType doorType)
     {
         Car.Doors.Add(new Door { Type = doorType });
         return (TSelf)this;
     }
 }
+
+public class AdvancedCarBuilder : AdvancedCarBuilder<AdvancedCarBuilder>
+{
+}
+
+/*
+// Post's example
+public class AdvancedCarBuilder : CarBuilder<AdvancedCarBuilder>
+{ 
+    // Once more, read it as "which returns ...",
+    // public class AdvancedCarBuilder : CarBuilder (which returns AdvancedCarBuilder)
+    // This way, methods inherited from CarBuilder, will return AdvancedCarBuilder.
+    // And AdvancedCarBuilder's methods will return AdvancedCarBuilder.
+    public AdvancedCarBuilder AddDoor(DoorType doorType)
+    { 
+        Car.Doors.Add(new Door { Type = doorType });
+        return this;
+    }
+}
+*/
--- RecursiveGenericsExample/RecursiveGenericsFluentBuilder/CarBuilder.cs
+++ RecursiveGenericsExample/RecursiveGenericsFluentBuilder/CarBuilder.cs
@@ -2,11 +2,19 @@
 
 namespace RecursiveGenericsFluentBuilder;
 
+// The idea is that we pass a type, that will extend CarBuilder
+// As the CarBuilder's generic parameter
 public class CarBuilder<TSelf> : BaseCarBuilder
     where TSelf : CarBuilder<TSelf>
-{
+{   // For example, imagine that TSelf is "AdvancedCarBuilder".
+    // Try to visually replace the <TSelf> with "which returns TSelf".
+    // It looks less confusing this way.
+    // Like:
+    // public class CarBuilder (which returns AdvancedCarBuilder) : BaseCarBuilder
+    // where AdvancedCarBuilder : CarBuilder (which returns AdvancedCarBuilder)
     private const int WheelsToAddAmount = 2;
 
+    // Using TSelf to specify the inheritor's return type. 
     public TSelf AddWheelsPair(int radius)
     {
         for (int i = 0; i < WheelsToAddAmount; i++)
@@ -14,8 +22,14 @@ public class CarBuilder<TSelf> : BaseCarBuilder
             Car.Wheels.Add(new Wheel { Radius = radius });
         }
 
+        // Knowing that TSelf (AdvancedCarBuilder) extends CarBuilder,
+        // Parse the return type from CarBuilder back to TSelf (AdvancedCarBuilder)
         return (TSelf)this;
     }
 }
 
+// Because we still need the "simple" CarBuilder class to be functional,
+// We can provide CarBuilder as the generic parameter to itself.
+// Read it as in the example above:
+// public class CarBuilder : CarBuilder (which returns CarBuilder)
 public class CarBuilder : CarBuilder<CarBuilder>{}
